58cede209ac61b2f9d7813fb7fd8688ebdeff170
Yousef Ayman <140937477+yousefjoo12@users.noreply.github.com>
2026-06-12 23:50:09 +0300

    Add Endpoints CreateUser GetUsers_NotActive GetUsers_Active DeleteUser =>Softdelete

--- Api/Controllers/AdminController.cs
+++ Api/Controllers/AdminController.cs
@@ -27,12 +27,79 @@ namespace API.Controllers
         }
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)] 
         [Authorize(Roles = "Admin")]
-        [HttpGet("GetUsers")]
-        public async Task<ActionResult<UserDTO>> GetUsers()
+        [HttpGet("GetUsers_Active")]
+        public async Task<ActionResult<UserDTO>> GetUsers_Active()
         {
             var users = await _userManager.Users.Where(X =>X.IsDeleted == false).ToListAsync();
             var  mapUser = _mapper.Map<List<UserResponseDTO>>(users);
             return Ok(mapUser);
         }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Authorize(Roles = "Admin")]
+        [HttpGet("GetUsers_NotActive")]
+        public async Task<ActionResult<UserDTO>> GetUsers_NotActive()
+        {
+            var users = await _userManager.Users.Where(X => X.IsDeleted == true).ToListAsync();
+            var mapUser = _mapper.Map<List<UserResponseDTO>>(users);
+            return Ok(mapUser);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Authorize(Roles = "Admin")]
+        [HttpPost("CreateUser")]
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserDTO model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var userExists = await _userManager.FindByEmailAsync(model.Email);
+            if (userExists != null) 
+            return BadRequest(new ApiResponse(400, "User already exists"));
+
+
+            var user = new AppUser
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                DisplayName = model.DisplayName,
+                UserType = model.UserType,
+                CreatedAt = DateTime.UtcNow,
+                IsDeleted = false
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(new ApiResponse(400, "Erorr while Create User"));
+
+            // Assign Role
+            await _userManager.AddToRoleAsync(user, model.UserType.ToString());
+
+            return Ok(new
+            {
+                message = "User created successfully",
+                userId = user.Id
+            });
+        }
+
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("DeleteUser/{id}")]
+        public async Task<IActionResult> DeleteUser( string id)
+        {
+            var user = await _userManager.FindByIdAsync(id); 
+            if (user == null)
+                return NotFound(new ApiResponse(404, "User not found"));
+
+            user.IsDeleted = true;
+            var result = await _userManager.UpdateAsync(user);
+
+            if (!result.Succeeded)
+                return BadRequest(new ApiResponse(400, "Erorr while deleting"));
+
+            return Ok(new ApiResponse(200, "User deleted successfully"));
+        }
+
     }
 }
---
+++ Api/DTOs/Identity/CreateUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.Identity
+{
+    public class CreateUserDTO
+    {
+        [Required] 
+        public string DisplayName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+
+        public UserType UserType { get; set; }
+        [Required]
+        public string Password { set; get; }
+    }
+}
